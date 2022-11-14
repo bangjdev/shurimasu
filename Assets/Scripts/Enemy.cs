c6ced93927e58ed0bbeda5a10e02d217a84102f2
Bang Nguyen <lelybang.nguyen@gmail.com>
2022-11-14 20:39:06 +0100

    Moving enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,6 +15,12 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.rigidBody.transform.position = Vector3.MoveTowards(this.rigidBody.transform.position, player.transform.position, Time.fixedDeltaTime * enemySpeed);
+        if (this.rigidBody.useGravity) {
+            this.rigidBody.transform.position = Vector3.MoveTowards(this.rigidBody.transform.position, player.transform.position, Time.fixedDeltaTime * enemySpeed);
+            var vectorTowardPlayer = player.transform.position - this.rigidBody.transform.position;
+            this.rigidBody.transform.rotation = Quaternion.RotateTowards(this.rigidBody.transform.rotation, Quaternion.LookRotation(vectorTowardPlayer), enemySpeed);
+        } else {
+            this.rigidBody.transform.Rotate(new Vector3(0, 1, 0) * enemySpeed * 2, Space.World);
+        }
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,10 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     private float spawnRate = .3f;
     public GameObject enemyPrefab;
-    public Transform spawnLocation;
 
     private float lastSpawnTime = 0;
     private float currentHealth = 100f;
@@ -20,7 +20,15 @@ public class EnemySpawner : MonoBehaviour
     {
         if ((Time.time - lastSpawnTime) >= 1.0 / spawnRate) {
             lastSpawnTime = Time.time;
-            Instantiate(enemyPrefab, spawnLocation.position, spawnLocation.rotation);
+            var position = new Vector3(Random.Range(-20, 20), 4, Random.Range(-20, 20));
+            StartCoroutine(Spawn(position));
         }
+    }
+
+    IEnumerator Spawn(Vector3 location) {
+        var enemy = Instantiate(enemyPrefab, location, this.transform.rotation);
+        enemy.GetComponent<Rigidbody>().useGravity = false;
+        yield return new WaitForSeconds(2);
+        enemy.GetComponent<Rigidbody>().useGravity = true;
     }
 }
